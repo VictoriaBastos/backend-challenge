ccb70517ff2d771c3a1d6cf68a30043f52fea053
Victoria <victoria.rezbastos@gmail.com>
2022-01-14 21:10:04 -0300

    Inserindo InputPort + Service

--- PasswordValidation/src/main/java/com/challenge/api/PasswordValidation/core/usecase/PasswordValidationService.java
+++ PasswordValidation/src/main/java/com/challenge/api/PasswordValidation/core/usecase/PasswordValidationService.java
@@ -1,4 +1,23 @@
 package com.challenge.api.PasswordValidation.core.usecase;
 
-public class PasswordValidationService {
+import com.challenge.api.PasswordValidation.core.domain.Password;
+import com.challenge.api.PasswordValidation.port.in.PasswordValidationInputPort;
+
+public class PasswordValidationService implements PasswordValidationInputPort {
+    @Override
+    public boolean validatePassword(Password password) {
+        String userPassword = password.getPassword();
+
+        boolean hasDigit = userPassword.replaceAll("[0-9]","").length() != userPassword.length();
+        boolean hasLowerCase = userPassword.replaceAll("[a-z]","").length() != userPassword.length();
+        boolean hasUpperCase = userPassword.replaceAll("[A-Z]","").length() != userPassword.length();
+        boolean hasSpecialCharacter = userPassword.replaceAll("[!@#$%^&*()-+]","").length() != userPassword.length();
+        boolean hasEnoughCharacters = userPassword.length() >= 9;
+        boolean noBlankSpace = userPassword.indexOf(" ") < 0;
+
+        if(hasDigit && hasLowerCase && hasUpperCase && hasSpecialCharacter && hasEnoughCharacters && noBlankSpace){
+            return true;
+        }
+        return false;
+    }
 }
--- PasswordValidation/src/main/java/com/challenge/api/PasswordValidation/port/in/PasswordValidationInputPort.java
+++ PasswordValidation/src/main/java/com/challenge/api/PasswordValidation/port/in/PasswordValidationInputPort.java
@@ -1,4 +1,7 @@
 package com.challenge.api.PasswordValidation.port.in;
 
+import com.challenge.api.PasswordValidation.core.domain.Password;
+
 public interface PasswordValidationInputPort {
+    boolean validatePassword(Password password);
 }
